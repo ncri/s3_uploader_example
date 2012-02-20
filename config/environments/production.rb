2f1cdc0e4c11f8bb360c09c1326f7474d568ecdb
Nico <ncrdevmail@gmail.com>
2012-02-21 00:08:59 +0100

    image path fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Html5S3Uploader::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
