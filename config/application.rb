76a103feb2d6d8f26a5c79b4a7cf2513e4154039
Nico <ncrdevmail@gmail.com>
2012-02-20 23:37:02 +0100

    debugging ie

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module Html5S3Uploader
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.compress = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
